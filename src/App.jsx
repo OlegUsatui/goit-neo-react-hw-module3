41cd1324b2c2c23b9f2914698a085576e9c931a1
oleg-usatyi <oleg_usatyi@ukr.net>
2024-09-23 19:41:25 +0200

    created Phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import './App.css'
+import './App.module.css';
+import { useState } from 'react';
 import ContactForm from './components/ContactForm/ContactForm.jsx';
 import SearchBox from './components/SearchBox/SearchBox.jsx';
 import ContactList from './components/ContactList/ContactList.jsx';
-import contacts from './contacts.json'
+import styles from './App.module.css';
+import initContacts from './contacts.json';
 
 function App() {
+  const [contacts, setContacts] = useState(initContacts);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    contact.number.includes(searchQuery)
+  );
+
+  const addContact = (contact) => {
+    const newContact = {
+      ...contact,
+      id: contacts.length
+    }
+    const contactsArr = [...contacts, newContact]
+    setContacts(contactsArr)
+  }
+  const deleteContact = (id) => {
+    const newContacts = contacts.filter(contact => contact.id !== id);
+    setContacts([...newContacts])
+  };
+
+  const searchContacts = (val) => {
+    setSearchQuery(val)
+  }
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
+      <ContactForm onSubmit={addContact}/>
+      <SearchBox value={searchQuery} handleChange={searchContacts} />
+      <ContactList list={filteredContacts} deleteContact={(id) => deleteContact(id)} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,18 @@
-const ContactList = () => {
-  return <div>Contact</div>
-}
+import Contact from '../Contact/Contact.jsx';
+import styles from './ContactList.module.css';
 
-export default ContactList
+const ContactList = ({list, deleteContact}) => {
+  return (
+    <ul className={styles.contactList}>
+      {list.map((contact) => (
+        <Contact
+          key={contact.id}
+          contact={contact}
+          handleDelete={(id) => deleteContact(id)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
